f72ee9d2dba2ba67cc54d46b5195b3e285692f53
Mike's Pub <mikespub@users.noreply.github.com>
2021-03-02 21:13:07 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createSagaMiddleware from "redux-saga";
-import { createLogger } from "redux-logger";
+//import { createLogger } from "redux-logger";
 import UsersList from "./pages/home/usersList";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
